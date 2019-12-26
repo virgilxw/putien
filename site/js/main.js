9c783a5d3edd90afb4ba509867a3b5cdc1ba5457
virgilxw <reubenwxw@gmail.com>
2019-12-27 03:41:37 +0800

    polygon of ancient now shows

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,10 @@
-var map = L.map('MainMap').setView([25.31, 119.12], 12)
+var map = L.map('MainMap').setView([25.35, 119.12], 12)
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
 
-var Ancient_Shoreline = new L.GeoJSON.AJAX("./geojson/1.Ancient_Shoreline.geojson").addTo(map);
+var Ancient_Shoreline = new L.GeoJSON.AJAX("./geojson/1.Ancient_Shoreline.geojson", {
+	fillColor: "#FF1654",
+	fillOpacity: "0.2",
+	color: "#F3FFBD"
+}).addTo(map);
