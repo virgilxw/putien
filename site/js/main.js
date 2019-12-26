6d104de3c3465c9b5505dedcac3208d1f426f82d
virgilxw <reubenwxw@gmail.com>
2019-12-27 03:27:16 +0800

    ajax works

--- site/js/main.js
+++ site/js/main.js
@@ -1,7 +1,6 @@
-var map = L.map('MainMap').setView([25.439, 119.010], 11);
-
+var map = L.map('MainMap').setView([25.31, 119.12], 12)
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
 
-var Ancient_Shoreline = new L.GeoJSON.AJAX("./geojson/1.Ancient_Shoreline.geojson");
+var Ancient_Shoreline = new L.GeoJSON.AJAX("./geojson/1.Ancient_Shoreline.geojson").addTo(map);
