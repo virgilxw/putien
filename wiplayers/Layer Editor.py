13e5c2c9b03f4b091010f0bd38394fd21e52049e
virgilxw <reubenwxw@gmail.com>
2020-05-22 20:44:31 +0800

    verified points 1-100

--- wiplayers/Layer Editor.py
+++ wiplayers/Layer Editor.py
@@ -2,8 +2,11 @@ village_layers = QgsProject.instance().mapLayersByName('Village_Points')
 village_layer = village_layers[0]
 villages = village_layer.getFeatures()
 
+print(buffer)
+
 village_layer.startEditing()
 for entry in buffer:
+    print(entry["index_village"])
     village_layer.changeAttributeValue(entry["id"], 4, entry["index_village"])
 #    village_layer.changeAttributeValue(1, 4 , "test")
 village_layer.commitChanges()
