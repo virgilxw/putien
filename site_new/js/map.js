ccfe7970c980831c5b17ebcb3985b844921bfee4
reuben <reubenwxw@gmail.com>
2020-07-28 02:46:11 +0800

    procession bar chart works

--- site_new/js/map.js
+++ site_new/js/map.js
@@ -314,4 +314,65 @@ $(document).ready(function () {
     $("#clear").click(function () {
         clearallUID();
     })
+
+    // yuanxiao chart
+    var margin = {
+            top: 15,
+            right: 15,
+            bottom: 50,
+            left: 15
+        },
+        width = 380 - margin.left - margin.right,
+        height = 300
+
+    var svg = d3.select("#ProcessionsChart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+    d3.json("/json/processions.json").then(function (data) {
+        data.forEach(function (d) {
+            d.weight = +d.weight
+        })
+
+        // Scale the range of the data in the domains
+        x.domain(data.map(function (d) {
+            return d.date;
+        }));
+        y.domain([0, d3.max(data, function (d) {
+            return d.weight;
+        })]);
+
+        // append the rectangles for the bar chart
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) {
+                return x(d.date);
+            })
+            .attr("width", x.bandwidth())
+            .attr("y", function (d) {
+                return y(d.weight);
+            })
+            .attr("height", function (d) {
+                return height - y(d.weight);
+            });
+
+        // add the x Axis
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+
+        // add the y Axis
+        svg.append("g");
+    })
 })
